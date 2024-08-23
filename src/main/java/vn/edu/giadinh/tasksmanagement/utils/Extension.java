5199c34f76c421a4782c4e19f8a4a298d9bc815a
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-24 03:48:37 +0700

    Added Extension class.

---
+++ src/main/java/vn/edu/giadinh/tasksmanagement/utils/Extension.java
@@ -0,0 +1,48 @@
+package vn.edu.giadinh.tasksmanagement.utils;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Collection;
+
+@UtilityClass
+public class Extension {
+    public void ifNotNull(Object target, Function function) {
+        ObjectUtil.ifNotNull(target, function);
+    }
+
+    public void ifNull(Object target, Function function) {
+        ObjectUtil.ifNull(target, function);
+    }
+
+    public void ifNullThrow(Object target, RuntimeException e) {
+        ObjectUtil.ifNullThrow(target, e);
+    }
+
+    public boolean isNullOrEmpty(Collection<?> target) {
+        return CollectionUtil.isNullOrEmpty(target);
+    }
+
+    public <T> T find(Collection<T> collection, Predicator<T> predicator) {
+        return CollectionUtil.find(collection, predicator);
+    }
+
+    public <T> T findOrThrow(Collection<T> collection, Predicator<T> predicator, RuntimeException e) {
+        return CollectionUtil.findOrThrow(collection, predicator, e);
+    }
+
+    public boolean isNullOrEmpty(String target) {
+        return StringUtil.isNullOrEmpty(target);
+    }
+
+    public <T> boolean isNullOrEmpty(T[] target) {
+        return ArrayUtil.isNullOrEmpty(target);
+    }
+
+    public <T> T find(T[] arr, Predicator<T> predicator) {
+        return ArrayUtil.find(arr, predicator);
+    }
+
+    public <T> T findOrThrow(T[] arr, Predicator<T> predicator, RuntimeException e) {
+        return ArrayUtil.findOrThrow(arr, predicator, e);
+    }
+}
