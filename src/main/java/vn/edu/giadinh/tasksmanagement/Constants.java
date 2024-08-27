809fac6686938a077ab5a924fba319ddcf336e4f
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-27 16:02:28 +0700

    Added Constants static class for storing constant values.

---
+++ src/main/java/vn/edu/giadinh/tasksmanagement/Constants.java
@@ -0,0 +1,14 @@
+package vn.edu.giadinh.tasksmanagement;
+
+public class Constants {
+    // Static fields:
+    public static final String MSG_INFORMATION_REQUIRED = "Vui lòng cung cấp đầy đủ thông tin cần thiết!";
+    public static final String MSG_LOGIN_INVALID = "Tên đăng nhập hoặc mật khẩu không đúng!";
+    public static final String MSG_ACCESS_DENIED = "Bạn không có quyền truy cập vào trang này!";
+    public static final String MSG_ALREADY_LOGGED_IN = "Bạn đã đăng nhập rồi!";
+    public static final String MSG_USER_NOT_FOUND = "Tài khoản không tồn tại!";
+
+
+    // Constructors:
+    private Constants() {}
+}
