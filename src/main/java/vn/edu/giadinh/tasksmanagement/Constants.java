9a09825820afba52c865333a89193ffcb65ff8c6
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-29 10:39:13 +0700

    Constants: + Added new messages

--- src/main/java/vn/edu/giadinh/tasksmanagement/Constants.java
+++ src/main/java/vn/edu/giadinh/tasksmanagement/Constants.java
@@ -9,6 +9,8 @@ public class Constants {
     public static final String MSG_USER_NOT_FOUND = "Tài khoản không tồn tại!";
     public static final String MSG_USER_NOT_LOGGED_IN = "Bạn chưa đăng nhập!";
     public static final String MSG_UPDATED_SUCCESSFULLY = "Cập nhật thành công!";
+    public static final String MSG_INCORRECT_ROLE = "Vai trò người dùng cung cấp không hợp lệ!";
+    public static final String MSG_USER_ALREADY_EXIST = "Người dùng này đã tồn tại!";
 
     // Constructors:
     private Constants() {}
