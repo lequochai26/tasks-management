dff275942ec979c0e26c076f1fb0c40dfd203cf7
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-29 10:40:59 +0700

    UserRole: + Added from static method.

--- src/main/java/vn/edu/giadinh/tasksmanagement/enums/UserRole.java
+++ src/main/java/vn/edu/giadinh/tasksmanagement/enums/UserRole.java
@@ -1,7 +1,31 @@
 package vn.edu.giadinh.tasksmanagement.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum UserRole {
     ADMIN,
     DEVELOPER,
-    QUALITY_CONTROL
+    QUALITY_CONTROL;
+
+    private static Map<String, UserRole> roleMap;
+
+    public static UserRole from(String name) {
+        if (getRoleMap() == null || !roleMap.containsKey(name)) {
+            return null;
+        }
+
+        return roleMap.get(name);
+    }
+
+    private static Map<String, UserRole> getRoleMap() {
+        if (roleMap == null) {
+            roleMap = new HashMap<>();
+            for (UserRole role : UserRole.values()) {
+                roleMap.put(role.name(), role);
+            }
+        }
+
+        return roleMap;
+    }
 }
