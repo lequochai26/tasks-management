c7b430bf7d8b214df07b6de23216b36ea728efc6
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-30 09:25:25 +0700

    TaskDBHandler: + Implements newly added ITaskDBHandler.

--- src/main/java/vn/edu/giadinh/tasksmanagement/daos/TaskDBHandler.java
+++ src/main/java/vn/edu/giadinh/tasksmanagement/daos/TaskDBHandler.java
@@ -1,16 +1,19 @@
 package vn.edu.giadinh.tasksmanagement.daos;
 
+import lombok.experimental.ExtensionMethod;
 import vn.edu.giadinh.tasksmanagement.converter.TaskConverter;
+import vn.edu.giadinh.tasksmanagement.converter.TasksListConverter;
 import vn.edu.giadinh.tasksmanagement.models.Task;
 import vn.edu.giadinh.tasksmanagement.utils.DBUtil;
+import vn.edu.giadinh.tasksmanagement.utils.Extension;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class TaskDBHandler implements DBHandler<Task, Integer> {
+@ExtensionMethod(Extension.class)
+public class TaskDBHandler implements ITaskDBHandler {
     // Static fields:
     public static final String GET_SQL = "SELECT * FROM task" +
             " WHERE id=?";
@@ -22,6 +25,16 @@ public class TaskDBHandler implements DBHandler<Task, Integer> {
             " WHERE id=?";
     public static final String DELETE_SQL = "DELETE FROM task" +
             " WHERE id=?";
+    public static final String SEARCH_SQL = "SELECT * FROM task" +
+            " WHERE id=? OR title LIKE ?";
+    public static final String EXISTS_SQL = "SELECT COUNT(*) FROM task" +
+            " WHERE id=?";
+    public static final String GET_BY_RESPONSIBILITY_SQL = "SELECT * FROM task" +
+            " WHERE responsibility=?";
+    public static final String GET_BY_TESTER_SQL = "SELECT * FROM task" +
+            " WHERE tester=?";
+    public static final String GET_BY_USER_SQL = "SELECT * FROM task" +
+            " WHERE tester=? OR responsibility=?";
 
     private static TaskDBHandler instance;
 
@@ -71,15 +84,8 @@ public class TaskDBHandler implements DBHandler<Task, Integer> {
 
                     ResultSet table = statement.executeQuery();
 
-                    List<Task> result = new ArrayList<>();
-                    while (table.next()) {
-                        result.add(
-                                TaskConverter.getInstance()
-                                        .convert(table)
-                        );
-                    }
-
-                    return result;
+                    return TasksListConverter.getInstance()
+                            .convert(table);
                 }
         );
     }
@@ -127,6 +133,97 @@ public class TaskDBHandler implements DBHandler<Task, Integer> {
                     statement.setInt(1, target.getId());
 
                     statement.executeUpdate();
+                }
+        );
+    }
+
+    @Override
+    public List<Task> search(String keyword) throws SQLException {
+        if (keyword.isNullOrEmpty()) {
+            return getAll();
+        }
+
+        return DBUtil.executeQuery(
+                connection -> {
+                    PreparedStatement statement = connection.prepareStatement(SEARCH_SQL);
+
+                    if (keyword.canConvertToInt()) {
+                        statement.setInt(1, Integer.parseInt(keyword));
+                    }
+                    else {
+                        statement.setInt(1, -1);
+                    }
+
+                    statement.setString(2, "%" + keyword + "%");
+
+                    ResultSet table = statement.executeQuery();
+
+                    return TasksListConverter.getInstance()
+                            .convert(table);
+                }
+        );
+    }
+
+    @Override
+    public boolean exists(Integer key) throws SQLException {
+        if (key == null) {
+            return false;
+        }
+
+        return DBUtil.executeQuery(
+                connection -> {
+                    PreparedStatement statement = connection.prepareStatement(EXISTS_SQL);
+                    statement.setInt(1, key);
+
+                    ResultSet table = statement.executeQuery();
+
+                    return table.next() && table.getInt(1) > 0;
+                }
+        );
+    }
+
+    @Override
+    public List<Task> getByResponsibility(String responsibility) throws SQLException {
+        return DBUtil.executeQuery(
+                connection -> {
+                    PreparedStatement statement = connection.prepareStatement(GET_BY_RESPONSIBILITY_SQL);
+                    statement.setString(1, responsibility);
+
+                    ResultSet table = statement.executeQuery();
+
+                    return TasksListConverter.getInstance()
+                            .convert(table);
+                }
+        );
+    }
+
+    @Override
+    public List<Task> getByTester(String tester) throws SQLException {
+        return DBUtil.executeQuery(
+                connection -> {
+                    PreparedStatement statement = connection.prepareStatement(GET_BY_TESTER_SQL);
+                    statement.setString(1, tester);
+
+                    ResultSet table = statement.executeQuery();
+
+                    return TasksListConverter.getInstance()
+                            .convert(table);
+                }
+        );
+    }
+
+    @Override
+    public List<Task> getByUser(String username) throws SQLException {
+        return DBUtil.executeQuery(
+                connection -> {
+                    PreparedStatement statement = connection.prepareStatement(GET_BY_USER_SQL);
+                    statement.setString(1, username);
+                    statement.setString(2, username);
+
+                    ResultSet table = statement.executeQuery();
+
+                    return TasksListConverter.getInstance()
+                            .convert(table);
                 }
         );
     }
