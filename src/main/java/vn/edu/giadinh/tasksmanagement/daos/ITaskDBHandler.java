9f3ca9ca4adffa85d73d1d4495d9e1f188930769
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-30 09:24:03 +0700

    Added ITaskDBHandler interface.

---
+++ src/main/java/vn/edu/giadinh/tasksmanagement/daos/ITaskDBHandler.java
@@ -0,0 +1,12 @@
+package vn.edu.giadinh.tasksmanagement.daos;
+
+import vn.edu.giadinh.tasksmanagement.models.Task;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ITaskDBHandler extends ExistsCheckableDBHandler<Task, Integer> {
+    List<Task> getByResponsibility(String responsibility) throws SQLException;
+    List<Task> getByTester(String tester) throws SQLException;
+    List<Task> getByUser(String username) throws SQLException;
+}
