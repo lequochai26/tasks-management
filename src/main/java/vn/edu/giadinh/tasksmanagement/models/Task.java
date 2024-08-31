ccbb3037e65041e11b82333361034e73ed9091de
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-31 16:58:26 +0700

    Added canAccess method for Task model.

--- src/main/java/vn/edu/giadinh/tasksmanagement/models/Task.java
+++ src/main/java/vn/edu/giadinh/tasksmanagement/models/Task.java
@@ -1,8 +1,11 @@
 package vn.edu.giadinh.tasksmanagement.models;
 
 import lombok.*;
+import lombok.experimental.ExtensionMethod;
 import vn.edu.giadinh.tasksmanagement.enums.TaskProgress;
 import vn.edu.giadinh.tasksmanagement.enums.TaskStatus;
+import vn.edu.giadinh.tasksmanagement.enums.UserRole;
+import vn.edu.giadinh.tasksmanagement.utils.Extension;
 
 @Builder
 @NoArgsConstructor
@@ -10,6 +13,7 @@ import vn.edu.giadinh.tasksmanagement.enums.TaskStatus;
 @Getter
 @Setter
 @ToString
+@ExtensionMethod(Extension.class)
 public class Task {
     // Fields:
     private int id;
@@ -19,4 +23,24 @@ public class Task {
     private TaskProgress progress;
     private String responsibility;
     private String tester;
+
+    // Methods:
+    public boolean canAccess(User user) {
+        if (user == null) {
+            return false;
+        }
+
+        return (
+                UserRole.ADMIN.equals(user.getRole())
+                || (
+                        !this.responsibility.isNullOrEmpty()
+                        && this.responsibility.equals(user.getUsername()))
+                        && UserRole.DEVELOPER.equals(user.getRole())
+                || (
+                        !this.tester.isNullOrEmpty()
+                        && this.tester.equals(user.getUsername())
+                        && UserRole.QUALITY_CONTROL.equals(user.getRole())
+                        )
+                );
+    }
 }
