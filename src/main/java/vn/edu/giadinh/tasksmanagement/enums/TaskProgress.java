2bf08aadfea90b088405d0a14dc903a7e46edb9b
Lê Quốc Hải <lequochai26@gmail.com>
2024-08-31 12:08:26 +0700

    TaskProgress: + Added allValues static field + Added from static method.

--- src/main/java/vn/edu/giadinh/tasksmanagement/enums/TaskProgress.java
+++ src/main/java/vn/edu/giadinh/tasksmanagement/enums/TaskProgress.java
@@ -1,10 +1,32 @@
 package vn.edu.giadinh.tasksmanagement.enums;
 
+import lombok.experimental.ExtensionMethod;
+import vn.edu.giadinh.tasksmanagement.utils.Extension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ExtensionMethod(Extension.class)
 public enum TaskProgress {
     ANALYZING,
     DEVELOPING,
     TEST_ATTEMPTING,
     TESTING,
     TEST_FAILED,
-    TEST_SUCCESS
+    TEST_SUCCESS;
+
+    // Static fields:
+    public static final List<TaskProgress> allValues = Arrays.stream(TaskProgress.values()).toList();
+
+    // Methods:
+    public static TaskProgress from(String name) {
+        if (name.isNullOrEmpty()) {
+            return null;
+        }
+
+        return allValues.find(
+                value -> value.name().equals(name)
+        );
+    }
 }
